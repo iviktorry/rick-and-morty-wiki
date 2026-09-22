3bc64b246a6dad0e2200a0123dd1f9c808fff22f
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-22 13:36:33 +0200

    feat: connect React Router with basic structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <header className="flex gap-4">
-      <h1>Rick & Morty Wiki</h1>
+      <h1>
+        <Link to="/">Rick & Morty Wiki</Link>
+      </h1>
       <nav>
         <ul className="flex gap-2">
-          <li>Characters</li>
-          <li>Episodes</li>
-          <li>Location</li>
+          <li>
+            <Link to="/characters">Characters</Link>
+          </li>
+          <li>
+            <Link to="/episodes">Episodes</Link>
+          </li>
+          <li>
+            <Link to="/locations">Location</Link>
+          </li>
         </ul>
       </nav>
     </header>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Locations from "../pages/Locations";
+import Episodes from "../pages/Episodes";
+import Characters from "../pages/Characters";
+
 export default function Main() {
-  return <main></main>;
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/locations" element={<Locations />} />
+        <Route path="/episodes" element={<Episodes />} />
+        <Route path="/characters" element={<Characters />} />
+      </Routes>
+    </main>
+  );
 }
