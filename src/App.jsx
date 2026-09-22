10b73621b6ca8c4125b9839c5d7508557bd16804
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-22 13:01:36 +0200

    feat: add fetch data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 export default function App() {
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  }, []);
+
   return (
     <div className="font-custom flex min-h-lvh flex-col bg-orange-100 tracking-wider">
       <Header />
