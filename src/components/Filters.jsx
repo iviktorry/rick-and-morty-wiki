c72f06ad92b132eeda61734e583cf98de4b8b976
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-23 14:06:24 +0200

    feat: add trial hardcoded structure to Filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,12 +1,35 @@
-import { Search } from "lucide-react";
+import FilterButton from "./FilterButton";
 
 export default function Filters() {
   return (
-    <form className="mx-auto w-fit lg:fixed lg:top-80 lg:left-0">
-      <div className="flex items-center gap-2">
-        <Search />
-        <input type="text" className="w-50 ring" />
+    <div className="w-full md:max-w-2xl lg:w-sm">
+      <p className="text-xl font-medium">Filters</p>
+      <div className="mt-2 rounded-lg">
+        <details className="border-b-2 border-neutral-800 py-2">
+          <summary>Status</summary>
+          <div className="flex flex-wrap items-start gap-2 py-2">
+            <FilterButton text="Alive" />
+            <FilterButton text="Dead" />
+            <FilterButton text="Unknown" />
+          </div>
+        </details>
+        <details className="border-b-2 border-neutral-800 py-2">
+          <summary className="marker:list-">Species</summary>
+          <div className="flex flex-wrap items-start gap-2 py-2">
+            <FilterButton text="Human" />
+            <FilterButton text="Alien" />
+            <FilterButton text="Unknown" />
+          </div>
+        </details>
+        <details className="border-b-2 border-neutral-800 py-2">
+          <summary>Gender</summary>
+          <div className="flex flex-wrap items-start gap-2 py-2">
+            <FilterButton text="Male" />
+            <FilterButton text="Female" />
+            <FilterButton text="Unknown" />
+          </div>
+        </details>
       </div>
-    </form>
+    </div>
   );
 }
