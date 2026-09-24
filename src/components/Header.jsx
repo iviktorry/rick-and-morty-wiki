88793910e75369129ad0a695ef92b3f91b248b59
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-24 17:39:44 +0200

    style: add logo image, add styles to navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,37 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 export default function Header() {
+  function navLinkStyles({ isActive }) {
+    return `transition-all duration-300 ease-linear md:hover:font-bold ${isActive ? "font-bold text-lg" : ""} `;
+  }
   return (
-    <header className="flex justify-between px-4 pt-4">
+    <header className="flex flex-col items-center justify-between px-4 sm:flex-row">
       <h1>
-        <Link to="/">Rick & Morty Wiki</Link>
+        <Link to="/">
+          <img
+            src={logo}
+            alt="rick and morty logo image"
+            className="max-w-55"
+          />
+        </Link>
       </h1>
       <nav>
-        <ul className="flex flex-col gap-4 sm:flex-row">
+        <ul className="flex flex-row items-center justify-between gap-4 sm:w-xs">
           <li>
-            <Link to="/character">Characters</Link>
+            <NavLink to="/character" className={navLinkStyles}>
+              Characters
+            </NavLink>
           </li>
           <li>
-            <Link to="/location">Location</Link>
+            <NavLink to="/location" className={navLinkStyles}>
+              Location
+            </NavLink>
           </li>
           <li>
-            <Link to="/episode">Episodes</Link>
+            <NavLink to="/episode" className={navLinkStyles}>
+              Episodes
+            </NavLink>
           </li>
         </ul>
       </nav>
