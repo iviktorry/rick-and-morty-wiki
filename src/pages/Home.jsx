337e99f57d136fb25031c458d7cd4e6df7e0f5db
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-25 18:43:33 +0200

    feat: fulfill home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <section></section>;
+  return (
+    <section className="flex flex-1 flex-col justify-center text-center">
+      <h2 className="text-2xl font-semibold md:text-3xl">
+        Welcome to the Rick and Morty Wiki!
+      </h2>
+      <p className="text-xl md:text-2xl">Choose topic you want to explore</p>
+    </section>
+  );
 }
